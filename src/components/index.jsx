b08f21d2232d79bf522487d113cbe175d7953228
andres64372 <77692131+andres64372@users.noreply.github.com>
2022-09-04 11:06:40 -0500

    deleted socketio

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useEffect,useState} from 'react';
 import {useNavigate} from "react-router-dom"
 import Picker from '../components/picker.jsx';
-import socketIOClient from "socket.io-client";
+//import socketIOClient from "socket.io-client";
 
 import Cookies from 'universal-cookie';
 import config from '../components/settings.json'
@@ -23,41 +23,36 @@ function Index(){
         history('/');
     }
 
-    function manage(payload){
-        SetDevice(prev => ({...prev,[payload.id]:{...prev[payload.id],[payload.branch]:payload.state}}))
-    }
+    // function manage(payload){
+    //     SetDevice(prev => ({...prev,[payload.id]:{...prev[payload.id],[payload.branch]:payload.state}}))
+    // }
 
     useEffect(()=>{
-        const client = socketIOClient(host);
+        //const client = socketIOClient(host);
         window.addEventListener("offline", () => {
             SetOffline(true);
         });
         window.addEventListener("online", () => {
             SetOffline(false);
         });
-        // client.on('connect_error', err => {
-        //     SetOffline(true)
-        // });
-        client.on('connect', ()=>{
-            fetch(`${host}devices`,{
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${cookies.get('token')}`
-                },
-            })
-            .then(res => res.json())
-            .then(response => {      
-            for(let device of response.list){
-                client.on(device,manage)
-            }  
-            SetDevice(response.states)
-            SetDeviceid(response.list)
-            // SetOffline(false)
-            })
-            // .catch(error => {
-            //     SetOffline(true)
-            // });
+        fetch(`${host}devices`,{
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${cookies.get('token')}`
+            },
         })
+        .then(res => res.json())
+        .then(response => {      
+        // for(let device of response.list){
+        //     client.on(device,manage)
+        // }  
+        SetDevice(response.states)
+        SetDeviceid(response.list)
+        // SetOffline(false)
+        })
+        // client.on('connect', ()=>{
+
+        // })
     },[])
     if(Offline){
         return(
